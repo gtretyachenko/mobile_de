3a884f61f4a3afb3494383b6a69552b8b90337a6
Admin <gtretyachenko@gmail.com>
2022-09-02 01:45:19 +0300

    Q

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import csv
-import requests
-from bs4 import BeautifulSoup
 import sys
 
+import requests
+from bs4 import BeautifulSoup
 
 params = {'page': 1}
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
 pages = params['page']
 
 url = 'https://www.mobile.de/ru/%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C/mercedes-benz-glc-220-d/vhc:car,srt:price,sro:asc,ms1:17200_253_d,frn:2017,frx:2019,ful:hybrid_diesel!diesel!lpg,mlx:125000'
@@ -83,47 +84,46 @@ while params['page'] <= pages:
                 res[count - 1].append(int(item_name.split('€')[0]))
             else:
                 res[count - 1].append(int(item_name.split('€')[0]))
-            # link_to_entry = 'https://www.mobile.de' + i.find('a').get('href')
-            # response = requests.get(link_to_entry, params=params, headers=headers)
-            # soup = BeautifulSoup(response.text, 'lxml')
-            # items_items = soup.find('div', class_='vip-details-block u-margin-bottom-18')
-            # res_res.append([params['page']])
-            # res_res[count - 1].append(n)
-            # for t, iii in enumerate(items_items.find('div', class_='attributes-box g-col-12')):
-            #     cont = iii.contents[1].text
-            #     if t == 5:
-            #         cont = cont.replace('км', '').replace(chr(160), '')
-            #     res_res[count - 1].append(cont)
-            # for t, iii in enumerate(items_items.find('div', class_='further-tec-data g-col-12')):
-            #     cont = iii.contents[1].text
-            #     if t == 0:
-            #         cont = cont.replace('ссм', '').replace(chr(160), '')
-            #     res_res[count - 1].append(cont)
-
+            link_to_entry = 'https://www.mobile.de' + i.find('a').get('href')
+            response = requests.get(link_to_entry, params=params, headers=headers)
+            soup = BeautifulSoup(response.text, 'lxml')
+            items_items = soup.find('div', class_='vip-details-block u-margin-bottom-18')
+            res_res.append([params['page']])
+            res_res[count - 1].append(n)
+            for t, iii in enumerate(items_items.find('div', class_='attributes-box g-col-12')):
+                cont = iii.contents[1].text
+                if t == 5:
+                    cont = cont.replace('км', '').replace(chr(160), '')
+                res_res[count - 1].append(cont)
+            for t, iii in enumerate(items_items.find('div', class_='further-tec-data g-col-12')):
+                cont = iii.contents[1].text
+                if t == 0:
+                    cont = cont.replace('ссм', '').replace(chr(160), '')
+                res_res[count - 1].append(cont)
 
         print(res[count - 1])
         # print(res_res[count - 1])
     num_page = params['page']
-    url = url.replace(f'pgn:{num_page}', f'pgn:{num_page+1}')
+    url = url.replace(f'pgn:{num_page}', f'pgn:{num_page + 1}')
     params['page'] += 1
 
 myData = res
 # myData2 = res_res
 with open('C:/Users/Admin/PycharmProjects/mobile_de/data_result.csv', 'w', newline='', encoding='utf-8') as csvfile:
     fieldnames = ['NumPage', 'NumLot', 'MarkerNew', 'LotName', 'ShopRating', 'CountReviews',
-        'Born(mm/yyyy)', 'ODO km', 'PriceEur', 'NetPriceEur']
+                  'Born(mm/yyyy)', 'ODO km', 'PriceEur', 'NetPriceEur']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     writer = csv.writer(csvfile)
     writer.writerows(myData)
     print("Writing complete")
 
-# with open('C:/Users/Admin/PycharmProjects/mobile_de/data_result2.csv', 'w', newline='', encoding='utf-8') as csvfile:
-#     fieldnames = ['Состояние', 'Категория', 'Год выпуска', 'Коробка передач', 'Топливо', 'Пробег',
-#         'Мощность', 'Объем двигателя', 'Кол-во мест', 'Кол-во дверей', 'Класс эко', 'Кол-во влад',
-#                   'Общий осмотр', 'Цвет', 'Состояние2', 'Категория', 'Номер ТС', 'Датчики парковки', 'Дизаин салон']
-#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#     writer.writeheader()
-#     writer = csv.writer(csvfile)
-#     writer.writerows(myData2)
-#     print("Writing complete")
+with open('C:/Users/Admin/PycharmProjects/mobile_de/data_result2.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    fieldnames = ['Состояние', 'Категория', 'Год выпуска', 'Коробка передач', 'Топливо', 'Пробег',
+        'Мощность', 'Объем двигателя', 'Кол-во мест', 'Кол-во дверей', 'Класс эко', 'Кол-во влад',
+                  'Общий осмотр', 'Цвет', 'Состояние2', 'Категория', 'Номер ТС', 'Датчики парковки', 'Дизаин салон']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer = csv.writer(csvfile)
+    writer.writerows(myData2)
+    print("Writing complete")
